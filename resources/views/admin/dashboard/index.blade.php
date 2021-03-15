f9f190b19326b50b0162aec445c094b15dc264ab
Rwhytm <rajuwahyudi842@gmail.com>
2021-03-15 14:09:01 +0700

    menyambungkan ke template dashboard

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,4 @@
+@extends('admin.layout')
+@section('content')
+    <h3>Dashboard</h3>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/admin/dashboard', 'Admin\DashboardController@index');a
+Route::group(
+    ['namespace' => 'Admin', 'prefix' => 'admin'],
+    function(){
+        Route::get('dashboard', 'DashboardController@index');
+    }
+);
