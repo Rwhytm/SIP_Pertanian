f4f396303c55abb910f646a5c37bf39734f05ebc
Rwhytm <rajuwahyudi842@gmail.com>
2021-03-31 23:43:35 +0700

    Stuck Add Categories

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -30,6 +30,9 @@
                         </tbody>
                     </table>
                 </div>
+                <div class="card-footer text-right">
+                    <a href="{{URL('admin/category/create')}}" class="btn btn-primary">Tambah Data</a>
+                </div>
             </div>
         </div>
     </div>
