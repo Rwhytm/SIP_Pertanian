c7ab01b68ac7864dd11249fcf8f002fe5a22596e
Rwhytm <rajuwahyudi842@gmail.com>
2021-03-31 21:32:00 +0700

    add migration pembeli and kategori

---
+++ database/migrations/2021_03_31_142023_create_pembeli_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePembeliTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pembeli', function (Blueprint $table) {
+            $table->id();
+            $table->string('email',30)->uniquekey();
+            $table->string('pssword',50);
+            $table->string('nama_pembeli',100);
+            $table->string('alamat',100);
+            $table->string('no_hp',13);
+            $table->string('profil',30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pembeli');
+    }
+}
