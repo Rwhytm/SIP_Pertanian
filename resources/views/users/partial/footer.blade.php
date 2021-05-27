6985719742d284c498eac79194597cdcf4d02005
Rwhytm <rajuwahyudi842@gmail.com>
2021-05-27 13:00:37 +0700

    perbaikan home users

--- resources/views/users/partial/footer.blade.php
+++ resources/views/users/partial/footer.blade.php
@@ -1,45 +1,95 @@
 <footer class="footer-area">
-    <div class="footer-top-area  pt-105 pb-65"  data-overlay="9">
+    <div class="footer-middle black-bg-2 pt-45 pb-15">
         <div class="container">
             <div class="row">
-                
-                <div class="col-lg-4">
-                    <div class="footer-widget mb-40">
-                        <h3 class="footer-widget-title">Address</h3>
-                        <div class="footer-contact">
-                            <p><span><i class="ti-location-pin"></i></span> 77 Seventh avenue USA 12555. </p>
+                <div class="col-lg-4 col-md-4">
+                    <div class="footer-services-wrapper mb-30">
+                        <div class="footer-services-icon">
+                            <i class="pe-7s-car"></i>
+                        </div>
+                        <div class="footer-services-content">
+                            <h3>Pernyataan 1</h3>
+                            <p>dasdwadwa</p>
                         </div>
                     </div>
                 </div>
-
-                <div class="col-lg-4">
-                    <div class="footer-widget mb-40">
-                        
+                <div class="col-lg-4 col-md-4">
+                    <div class="footer-services-wrapper mb-30">
+                        <div class="footer-services-icon">
+                            <i class="pe-7s-shield"></i>
+                        </div>
+                        <div class="footer-services-content">
+                            <h3>Pernyataan 2</h3>
+                            <p>dawdwad</p>
+                        </div>
                     </div>
                 </div>
-
-
-                <div class="col-lg-4">
-                    <div class="footer-widget mb-40">
-                        <h3 class="footer-widget-title">Customer Service</h3>
-                            
-                        <div class="footer-contact">
-                            
-                            <p><span><i class=" ti-headphone-alt "></i></span> +88 (015) 609735 or +88 (012) 112266</p>
+                <div class="col-lg-4 col-md-4">
+                    <div class="footer-services-wrapper mb-30">
+                        <div class="footer-services-icon">
+                            <i class="pe-7s-headphones"></i>
+                        </div>
+                        <div class="footer-services-content">
+                            <h3>Pernyataan 3</h3>
+                            <p>dawdawd</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="footer-bottom black-bg ptb-20">
+    <div class="footer-top-3 black-bg pt-75 pb-10">
         <div class="container">
             <div class="row">
-                <div class="col-12 text-center">
-                    <div class="copyright">
+                <div class="col-lg-4 col-md-6 col-xl-4">
+                    <div class="footer-widget mb-30">
+                        <h3 class="footer-widget-title-3">Hubungi kami</h3>
+                        <div class="footer-info-wrapper-2">
+                            <div class="footer-address-electro">
+                                <div class="footer-info-icon2">
+                                    <span>Alamat:</span>
+                                </div>
+                                <div class="footer-info-content2">
+                                    <p>77 Seventh Streeth Banasree <br>Road Rampura -2100 Dhaka</p>
+                                </div>
+                            </div>
+                            <div class="footer-address-electro">
+                                <div class="footer-info-icon2">
+                                    <span>Kontak:</span>
+                                </div>
+                                <div class="footer-info-content2">
+                                    <p>+11 (019)  2518  4203 </p>
+                                </div>
+                            </div>
+                            <div class="footer-address-electro">
+                                <div class="footer-info-icon2">
+                                    <span>Email:</span>
+                                </div>
+                                <div class="footer-info-content2">
+                                    <p><a href="#">domain@mail.com</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom  black-bg pt-25 pb-30">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-5">
+                    <div class="footer-menu">
+                        
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-7">
+                    <div class="copyright f-right mrg-5">
                         <p>
                             Copyright ©
-                            <a href="http://fp.unib.ac.id/">Fakultas Pertanian</a> 2021 . All Right Reserved.
+                            <a href="http://fp.unib.ac.id/">Fakultas Pertanian</a>
+                            2021 . All Right Reserved.
                         </p>
                     </div>
                 </div>
--- resources/views/users/partial/header.blade.php
+++ resources/views/users/partial/header.blade.php
@@ -1,46 +1,4 @@
-<header>
-    <div class="header-top-furniture wrapper-padding-2 res-header-sm">
-        <nav class="navbar navbar-light bg-white justify-content-between">
-            <!-- Navbar content -->
-            <a class="navbar-brand">{{Str::upper(config('app.name'))}}</a>
-            <form class="form-inline my-2 my-lg-0">
-                @if(Route::has('login'))
-                @auth
-                    
-                
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        {{'Hai, kak '.Auth::user()->nama }} 
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                      <a class="dropdown-item" href="#">profil</a>
-                      <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                        </a>
-                    </div>
-                </li>
-                
-                    
-                    @else
-                        <a href="{{ route('login2') }}">Login</a>
-
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}">Register</a>
-                            @endif
-                @endauth
-            @endif
-                
-            </form>
-        </nav>
-                    {{-- <a href="home.html">
-                        <img src="" alt="" style="width: 167px">
-                    </a> --}}
-                {{-- </div>
-                <div class="menu-style-2 furniture-menu menu-hover">
-                    <nav>
-                        <ul>
+{{-- 
                             
                             <li><a href="#">filter</a>
                                 <ul class="single-dropdown">
@@ -51,8 +9,34 @@
                                     <li><a href="{{route('hargatinggi')}}">Harga Tinggi Ke Rendah</a></li>
                                      <li><a href="{{route('hargarendah')}}">Harga Rendah Ke Tinggi</a></li>
                                 </ul>
+                            </li> --}}
+<header>
+    <div class="header-bottom wrapper-padding-2 bg-img res-header-sm food-header" style="background-image: url(assets/img/bg/11.jpg)">
+    <div class="header-top-furniture wrapper-padding-2 res-header-sm">
+        <div class="container-fluid">
+            <div class="header-bottom-wrapper">
+                <div class="logo-2 furniture-logo ptb-30">
+                    {{-- <a href="index.html">
+                        <img src="assets/img/logo/2.png" alt="" style="width: 167px">
+                    </a> --}}
+                </div>
+                <div class="menu-style-2 furniture-menu menu-hover">
+                    <nav>
+                        <ul>
+                            
+                            <li><a href="#">pages</a>
+                                <ul class="single-dropdown">
+                                    <li><a href="about-us.html">about us</a></li>
+                                    <li><a href="menu-list.html">menu list</a></li>
+                                    <li><a href="login.html">login</a></li>
+                                    <li><a href="register.html">register</a></li>
+                                    <li><a href="cart.html">cart page</a></li>
+                                    <li><a href="checkout.html">checkout</a></li>
+                                    <li><a href="wishlist.html">wishlist</a></li>
+                                    <li><a href="contact.html">contact</a></li>
+                                </ul>
                             </li>
-                            <li><a href="shop.html">my orders</a>
+                            <li><a href="shop.html">shop</a>
                                 <div class="category-menu-dropdown shop-menu">
                                     <div class="category-dropdown-style category-common2 mb-30">
                                         <h4 class="categories-subtitle"> shop layout</h4>
@@ -89,7 +73,7 @@
                                     </div>
                                 </div>
                             </li>
-                            <li><a href="blog.html">start a jastip</a>
+                            <li><a href="blog.html">blog</a>
                                 <ul class="single-dropdown">
                                     <li><a href="blog.html">blog 3 colunm</a></li>
                                     <li><a href="blog-2-col.html">blog 2 colunm</a></li>
@@ -98,15 +82,110 @@
                                     <li><a href="blog-details-sidebar.html">blog details 2</a></li>
                                 </ul>
                             </li>
-                            <li><a href="checkout.html">my wallet</a></li>
+                            <li><a href="contact.html">contact</a></li>
                         </ul>
                     </nav>
                 </div>
-                {{-- Space buat logout --}}
-                <div class="Logout">
-                    
+                <div class="header-cart">
+                    <a class="icon-cart-furniture" href="#">
+                        <i class="ti-shopping-cart"></i>
+                        <span class="shop-count-furniture green"></span>
+                    </a>
+                    <ul class="cart-dropdown">
+                        <li class="single-product-cart">
+                            {{-- <div class="cart-img">
+                                <a href="#"><img src="assets/img/cart/1.jpg" alt=""></a>
+                            </div> --}}
+                            <div class="cart-title">
+                                <h5><a href="#"> Bits Headphone</a></h5>
+                                <h6><a href="#">Black</a></h6>
+                                <span>$80.00 x 1</span>
+                            </div>
+                            <div class="cart-delete">
+                                <a href="#"><i class="ti-trash"></i></a>
+                            </div>
+                        </li>
+                        <li class="cart-space">
+                            <div class="cart-sub">
+                                <h4>Subtotal</h4>
+                            </div>
+                            <div class="cart-price">
+                                <h4>$240.00</h4>
+                            </div>
+                        </li>
+                        <li class="cart-btn-wrapper">
+                            <a class="cart-btn btn-hover" href="#">view cart</a>
+                            <a class="cart-btn btn-hover" href="#">checkout</a>
+                        </li>
+                    </ul>
                 </div>
-
+            </div>
+            <div class="row">
+                <div class="mobile-menu-area d-md-block col-md-12 col-lg-12 col-12 d-lg-none d-xl-none">
+                    <div class="mobile-menu">
+                        <nav id="mobile-menu-active">
+                            <ul class="menu-overflow">
+                                <li><a href="#">HOME</a>
+                                    <ul>
+                                        <li><a href="index.html">Fashion</a></li>
+                                        <li><a href="index-fashion-2.html">Fashion style 2</a></li>
+                                        <li><a href="index-fruits.html">Fruits</a></li>
+                                        <li><a href="index-book.html">book</a></li>
+                                        <li><a href="index-electronics.html">electronics</a></li>
+                                        <li><a href="index-electronics-2.html">electronics style 2</a></li>
+                                        <li><a href="index-food.html">food & drink</a></li>
+                                        <li><a href="index-furniture.html">furniture</a></li>
+                                        <li><a href="index-handicraft.html">handicraft</a></li>
+                                        <li><a href="index-smart-watch.html">smart watch</a></li>
+                                        <li><a href="index-sports.html">sports</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">pages</a>
+                                    <ul>
+                                        <li><a href="about-us.html">about us</a></li>
+                                        <li><a href="menu-list.html">menu list</a></li>
+                                        <li><a href="login.html">login</a></li>
+                                        <li><a href="register.html">register</a></li>
+                                        <li><a href="cart.html">cart page</a></li>
+                                        <li><a href="checkout.html">checkout</a></li>
+                                        <li><a href="wishlist.html">wishlist</a></li>
+                                        <li><a href="contact.html">contact</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">shop</a>
+                                    <ul>
+                                        <li><a href="shop-grid-2-col.html"> grid 2 column</a></li>
+                                        <li><a href="shop-grid-3-col.html"> grid 3 column</a></li>
+                                        <li><a href="shop.html">grid 4 column</a></li>
+                                        <li><a href="shop-grid-box.html">grid box style</a></li>
+                                        <li><a href="shop-list-1-col.html"> list 1 column</a></li>
+                                        <li><a href="shop-list-2-col.html">list 2 column</a></li>
+                                        <li><a href="shop-list-box.html">list box style</a></li>
+                                        <li><a href="product-details.html">tab style 1</a></li>
+                                        <li><a href="product-details-2.html">tab style 2</a></li>
+                                        <li><a href="product-details-3.html"> tab style 3</a></li>
+                                        <li><a href="product-details-4.html">sticky style</a></li>
+                                        <li><a href="product-details-5.html">sticky style 2</a></li>
+                                        <li><a href="product-details-6.html">gallery style</a></li>
+                                        <li><a href="product-details-7.html">gallery style 2</a></li>
+                                        <li><a href="product-details-8.html">fixed image style</a></li>
+                                        <li><a href="product-details-9.html">fixed image style 2</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">BLOG</a>
+                                    <ul>
+                                        <li><a href="blog.html">blog 3 colunm</a></li>
+                                        <li><a href="blog-2-col.html">blog 2 colunm</a></li>
+                                        <li><a href="blog-sidebar.html">blog sidebar</a></li>
+                                        <li><a href="blog-details.html">blog details</a></li>
+                                        <li><a href="blog-details-sidebar.html">blog details 2</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="contact.html"> Contact  </a></li>
+                            </ul>
+                        </nav>							
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -114,6 +193,34 @@
         <div class="container-fluid">
             <div class="furniture-bottom-wrapper">
                 <div class="furniture-login">
+                    @if (Route::has('login'))
+                    @auth
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {{'Selamat datang '.Auth::user()->nama }} 
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Profil</a>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                            </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                        </div>
+                    </li>
+                    
+                    @else
+                    
+                    <ul>
+                        <li><a href="login.html">Masuk </a></li>
+                        <li><a href="register.html">Registrasi </a></li>
+                    </ul>
+                    @endauth
+                @endif
                 </div>
                 <div class="furniture-search">
                     <form action="#">
@@ -123,10 +230,11 @@
                         </button>
                     </form>
                 </div>
-                <div class="furniture-login">
+                <div class="furniture-wishlist">
+                    
                 </div>
             </div>
         </div>
     </div>
-   
+</div>
 </header>
